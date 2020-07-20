6ff4abd6ebd0047920bcfc6dc7d5ee787da3f8c3
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-07-20 17:07:18 -0300

    Final

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -26,19 +26,16 @@ public class CarrinhoResource {
 	@Path("{id}")
     @GET
     @Produces(MediaType.APPLICATION_XML)
-    public String busca(@PathParam("id") long id) {
+    public Carrinho busca(@PathParam("id") long id) {
         Carrinho carrinho = new CarrinhoDAO().busca(id);
-        if (carrinho != null) {
-        	return carrinho.toXML();
-        } else {
-        	return "Não encontrado.";
-        }
+        
+        return carrinho;
+        
     }
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_XML)
-	public Response adiciona(String conteudo) {		
-		Carrinho carrinho = (Carrinho)new XStream().fromXML(conteudo);
+	public Response adiciona(Carrinho carrinho) {		
 		new CarrinhoDAO().adiciona(carrinho);
 		URI uri = URI.create("/carrinhos/" + carrinho.getId());
 		return Response.created(uri).build(); 
@@ -54,9 +51,8 @@ public class CarrinhoResource {
 	
 	@Path("{id}/produtos/{produtoId}/quantidade")
 	@PUT
-	public Response alteraProduto(String conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId) {
+	public Response alteraProduto(Produto conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
-		Produto produto = (Produto) new XStream().fromXML(conteudo);
 		carrinho.trocaQuantidade(produto);
 		return Response.ok().build();			
 	}
--- loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -52,10 +52,8 @@ public class ClienteTest {
 	
 	@Test
 	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/carrinhos/1").request().get(String.class);
-		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+
+		Carrinho carrinho = target.path("/carrinhos/1").request().get(Carrinho.class);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 	}	
 	
@@ -65,14 +63,13 @@ public class ClienteTest {
         carrinho.adiciona(new Produto(314L, "Microfone", 999, 1));
         carrinho.setRua("Rua Vergueiro");
         carrinho.setCidade("Sao Paulo");
-        String xml = carrinho.toXML();
-        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+        Entity<Carrinho> entity = Entity.entity(carrinho, MediaType.APPLICATION_XML);
 
         Response response = target.path("/carrinhos").request().post(entity);
         Assert.assertEquals(201, response.getStatus());
         String location = response.getHeaderString("Location");
-        String conteudo = client.target(location).request().get(String.class);
-        Assert.assertTrue(conteudo.contains("Microfone"));
+        Carrinho carrinhoCarregado = client.target(location).request().get(Carrinho.class);
+        Assert.assertEquals("Microfone", carrinhoCarregado.getProdutos().get(0).getNome());
     }
 
 }
